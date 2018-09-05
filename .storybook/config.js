58045c75207d2d74942984ae1ddbd1354bc9e266
Leandro Marques <leandrolmarques@gmail.com>
2018-09-05 16:39:59 -0300

    Configure project

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,41 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { setDefaults } from '@storybook/addon-info'
+import { withKnobs } from '@storybook/addon-knobs'
+import { setOptions } from '@storybook/addon-options'
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
+// Sets the info addon's options.
+setDefaults({
+  header: false,
+})
+
+setOptions({
+  hierarchySeparator: /\//,
+  hierarchyRootSeparator: /\|/,
+  name: 'Plurall UI',
+  url: 'https://gitlab.com/sdk12/plurall/plurall-ui',
+})
+
+const req = require.context('../src/components', true, /\.story\.(j|t)sx$/)
+const withStoryStyles = storyFn => {
+  return (
+    <div
+      style = {{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      { storyFn() }
+    </div>
+  )
 }
 
-configure(loadStories, module);
+const loadStories = () => {
+  addDecorator(withKnobs)
+  addDecorator(withStoryStyles)
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const include = path.resolve(__dirname, '../');
+
+module.exports = {
+  // Add '.ts' and '.tsx' as resolvable extensions.
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [{
+      test: /\.tsx/,
+      loader: 'babel-loader!ts-loader',
+      exclude: /node_modules/,
+      include
+    }]
+  }
+};
